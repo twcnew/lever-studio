71a594398c7618fc3e0df6894020aeeed6e41b59
deploy <deploy@local>
2026-07-23 01:38:02 +0200

    Deploy 2026-07-22T23:38:02Z

---
+++ _next/static/It2-p55WjaOjm6RXXOnHv/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/lever-studio//_next/:path+",
+        "destination": "/lever-studio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
